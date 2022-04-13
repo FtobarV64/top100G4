e852a8a3a4ce7db1d53cc9d2f6174568cd807273
FtobarV64 <ftobarv@gmail.com>
2022-04-13 10:16:18 -0400

    pregunta 1 commit

---
+++ top-100.sql
@@ -0,0 +1,12 @@
+-- Descripción
+-- Se requiere crear un sitio web dedicado al mundo cinematográfico donde los usuarios
+-- puedan buscar detalles del top 100 de películas más populares. El plus más importante de
+-- este sitio web debe ser la variedad de filtros que ofrece para una búsqueda más efectiva.
+-- Para este desafío necesitarás crear 2 tablas llamadas películas y reparto, sus datos los
+-- consigues en los ficheros de extensión csv ubicados en el Apoyo Desafío.
+-- Requerimientos
+-- 1. Crear una base de datos llamada películas. (1 Punto)
+\c ftobarv
+DROP DATABASE peliculas;
+CREATE DATABASE peliculas;
+\c peliculas
